4960a6d0e66356e2553a3687377b0a0bfbc582d5
Patrick Côté <85751624+cotpat@users.noreply.github.com>
2024-03-12 13:32:06 +0100

    more stuff

--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
 asciidoctor-pdf -a pdf-theme=theme.yml -a pdf-themesdir=resources/themes -a pdf-fontsdir=resources/fonts main.adoc
+asciidoctor -a pdf-theme=theme.yml -a pdf-themesdir=resources/themes -a pdf-fontsdir=resources/fonts main.adoc
